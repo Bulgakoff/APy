e543ad63a8da348d5effb618739ca4577a3a3aa2
Bulgakoff <hlbu@yandex.ru>
2020-01-01 16:23:02 +0300

    kkkfkfkfkfkfkkf

--- lesson03_ternar/tern.py
+++ lesson03_ternar/tern.py
@@ -8,3 +8,17 @@ for i in range(len(word)):
     result = ''.join(word1)
 print(word1)
 print(result)
+# //////////////////////////////////////////////////////////////////
+# создать список квадрратов чисел
+num = [1, 2, 3, 4, 3, 4, 5, 7, 4]
+num1 = [p ** 2 for p in num]
+print(num1)
+
+# список име на букву А
+names = ['Фывпу', 'Аррпок', 'Щдаоар', 'Алло']
+names1 = [p for p in names if 'А' in p]
+
+print(names1)
+a = [1, 2, 3]
+b = a[:]
+print(b)
